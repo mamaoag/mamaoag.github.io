ca46e78cc8f93eb63a123b599736e8399d746e22
Jio Mamaoag <jiomamaoag@gmail.com>
2023-01-31 13:13:54 +0800

    fixed favicon and block

--- components/About/Blocks/Experience.vue
+++ components/About/Blocks/Experience.vue
@@ -1,12 +1,17 @@
 <template>
-  <div></div>
+  <div class="relative border-2 border-black p-4 group h-48 w-48 group hover:cursor-pointer mt-2 ease-in">
+      <img :src="experience.image" class="relative h-full w-full grayscale group-hover:0 group-hover:grayscale-0" :alt="experience.company">
+      <div class="absolute inset-x-0 bottom-0 hidden text-center group-hover:block w-full p-2">
+        <h1 class="text-xs tracking-[3.5%] font-bold">{{ experience.title }} ({{experience.active}})</h1>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'AboutExperience',
   props: {
-    project: Object,
+    experience: Object,
   },
 }
 </script>
--- components/About/ExperienceBlock.vue
+++ components/About/ExperienceBlock.vue
@@ -1,35 +1,43 @@
 <template>
   <div class="w-full mt-8">
     <h3 class="text-lg tracking-[3.5%] underline">My Experience</h3>
-    <Showcase
-      v-for="project in showcase"
-      :key="project.title"
-      :project="project"
-    />
+    <div class="flex justify-start items-center space-x-4">
+      <Experience
+        v-for="experience in showcase"
+        :key="experience.title"
+        :experience="experience"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import Showcase from '../Home/Portfolio/Showcase'
+import Experience from './Blocks/Experience';
 
 export default {
   components: {
-    Showcase,
+    Experience
   },
   data() {
     return {
       showcase: [
         {
-          title: 'The Frazzled Cook',
-          img: '/portfolio/tfc.png',
-          category: 'Client Project / e-Commerce',
-          url: '/portfolio/the-frazzled-cook',
+          title: 'IT Specialist',
+          image: '/experience/zpc.png',
+          active: '2022 - Current',
+          company: 'Zuellig Pharma',
         },
         {
-          title: 'Binosearch',
-          img: '/portfolio/bnc.png',
-          category: 'Personal Project / Security Tool',
-          url: '/portfolio/binosearch',
+          title: 'Software Engineer',
+          image: '/experience/crh.jpg',
+          active: '2020 - 2022',
+          company: 'CREATIVEHOPE PH INC.',
+        },
+        {
+          title: 'Instructor I',
+          image: '/experience/feutech.png',
+          active: '2018 - 2022',
+          company: 'FEU Institute of Technology',
         },
       ],
     }
